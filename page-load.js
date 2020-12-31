5e5481f253a255f68fe391ab28d0ed0244ba7993
sanglade <76445139+sanglade@users.noreply.github.com>
2020-12-31 08:34:38 -0500

    Update page-load.js

--- page-load.js
+++ page-load.js
@@ -7,6 +7,6 @@ document.onreadystatechange = function () {
          document.getElementById('interactive');
          document.getElementById('load').style.visibility="hidden";
          document.getElementById('contents').style.visibility="visible";
-      },1000);
+      },500);
   }
 }
